b72aa7f24bfb1c95c7f48ba3ebecec8a61ac9bc4
Trevor Royer <troyer@redhat.com>
2023-08-21 11:46:50 -0700

    adding test examples

--- working-with-data-science-pipelines/part3/add_pipeline_executed.py
+++ working-with-data-science-pipelines/part3/add_pipeline_executed.py
@@ -19,6 +19,12 @@ def add_pipeline(a="1", b="7"):
     second_add_step = add_op(first_add_step.output, b)
 
 if __name__ == "__main__":
-    kfp_tekton.compiler.TektonCompiler().compile(
-        add_pipeline, package_path=__file__.replace(".py", ".yaml")
+    client = kfp_tekton.TektonClient(
+        host="https://ds-pipeline-pipelines-definition-pipeline-demo.apps.cluster-q9bcd.q9bcd.sandbox493.opentlc.com",
+        existing_token="sha256~ZenMMDSu6YD6GAjv49alq2kkGebpCoLb7crAZcXk5Do",
+    )
+
+    arguments = {"a": "7", "b": "8"}
+    client.create_run_from_pipeline_func(
+        add_pipeline, arguments=arguments, experiment_name="submitted-example"
     )
--- working-with-data-science-pipelines/part3/add_pipeline_upload.py
+++ working-with-data-science-pipelines/part3/add_pipeline_upload.py
@@ -3,7 +3,7 @@ import kfp_tekton
 
 def add(a: float, b: float) -> float:
     """Calculate the sum of the two arguments."""
-    return a + b
+    return a - b
 
 add_op = kfp.components.create_component_from_func(
     add,
@@ -19,6 +19,21 @@ def add_pipeline(a="1", b="7"):
     second_add_step = add_op(first_add_step.output, b)
 
 if __name__ == "__main__":
+    client = kfp_tekton.TektonClient(
+        host="https://ds-pipeline-pipelines-definition-pipeline-demo.apps.cluster-q9bcd.q9bcd.sandbox493.opentlc.com",
+        existing_token="sha256~ZenMMDSu6YD6GAjv49alq2kkGebpCoLb7crAZcXk5Do",
+    )
+
     kfp_tekton.compiler.TektonCompiler().compile(
         add_pipeline, package_path=__file__.replace(".py", ".yaml")
     )
+
+    pipeline_id = client.get_pipeline_id(__file__.replace(".py", ".yaml"))
+
+    pipeline_version = client.upload_pipeline_version(
+        __file__.replace(".py", ".yaml"), 
+        pipeline_version_name = "test2", 
+        pipeline_id = pipeline_id,
+    )
+
+    print(client.list_pipeline_versions(pipeline_id=pipeline_id))
