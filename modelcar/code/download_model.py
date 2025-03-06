5edce7ebabc08dbf3c3567fb0d19138cc924cb41
Trevor Royer <troyer@redhat.com>
2025-03-06 15:22:13 -0700

    minor fixes

--- modelcar/code/download_model.py
+++ modelcar/code/download_model.py
@@ -1,9 +1,9 @@
 from huggingface_hub import snapshot_download
 
-model_repo = "ibm-granite/granite-3.0-2b-instruct"
-
+# Specify the Hugging Face repository containing the model
+model_repo = "ibm-granite/granite-3.1-2b-instruct"
 snapshot_download(
     repo_id=model_repo,
-    local_dir="./models",
+    local_dir="/models",
     allow_patterns=["*.safetensors", "*.json", "*.txt"],
 )
